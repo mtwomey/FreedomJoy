519eef2218a1d855950bb442ac7bf9ca0438d483
Matthew Twomey <mtwomey@beakstar.com>
2016-09-25 20:54:41 -0500

    Add some "stickiness" to a 4-way POV used as buttons to keep the same value if you rock your thumb off to one side by 45 degrees

--- FreedomJoy/Controllers/PovButton.cs
+++ FreedomJoy/Controllers/PovButton.cs
@@ -10,11 +10,30 @@
 
         private readonly int _onValue;
         private readonly Pov _povParent;
+        private int _lastValue;
         public override bool State
         {
             get
             {
                 int value = _povParent.Value;
+                if (_lastValue == 0 & (value == 0 | value == 4500 | value == 31500)) // Add some "stickiness" to a 4-way POV used as buttons to keep the same value if you rock your thumb off to one side by 45 degrees
+                {
+                    value = 0;
+                }
+                if (_lastValue == 9000 & (value == 9000 | value == 4500 | value == 13500))
+                {
+                    value = 9000;
+                }
+                if (_lastValue == 18000 & (value == 18000 | value == 13500 | value == 22500))
+                {
+                    value = 18000;
+                }
+                if (_lastValue == 27000 & (value == 27000 | value == 22500 | value == 31500))
+                {
+                    value = 27000;
+                }
+
+                _lastValue = value;
                 return (value == _onValue);
             }
         }
